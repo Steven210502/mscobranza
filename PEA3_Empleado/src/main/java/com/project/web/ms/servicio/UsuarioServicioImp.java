8b4661c922684e6a7f17b3b7fd12847a2d53b0f8
Steven210502 <92065456+Steven210502@users.noreply.github.com>
2021-10-12 18:43:49 -0500

    Add files via upload

---
+++ PEA3_Empleado/src/main/java/com/project/web/ms/servicio/UsuarioServicioImp.java
@@ -0,0 +1,86 @@
+package com.project.web.ms.servicio;
+
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.project.web.ms.modelo.Roles;
+import com.project.web.ms.modelo.Usuario;
+import com.project.web.ms.repositorio.UsuarioRepositorio;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class UsuarioServicioImp implements UsuarioServicio{
+	
+	public final UsuarioRepositorio usuarioRepositorio;
+	
+	@Override
+	public List<Usuario> ListAllUsuario() {
+		return usuarioRepositorio.findAll();
+	}
+
+	@Override
+	public Usuario getUsuario(Long id) {
+		return usuarioRepositorio.findById(id).orElse(null);
+	}
+
+	@Override
+	public Usuario createUsuario(Usuario usuario) {
+		usuario.setNombre("Mario"); 
+		usuario.setTipodocumento("DNI");
+		usuario.setNumdocumento("7418523");
+		usuario.setDireccion("JR.MARIN 15");
+		usuario.setTelefono("987654321");
+		usuario.setEmail("email@gmail.com");
+		usuario.setClave("123");
+		usuario.setEstado(1);
+		return usuarioRepositorio.save(usuario);
+	}
+
+	@Override
+	public Usuario updateUsuario(Usuario usuario) {
+		Usuario usuarioUpdate = getUsuario(usuario.getIdusuario());
+		
+		if(usuarioUpdate == null) {
+			return null;
+		}
+		//accesoUpdate.setRoles (acceso.getRoles());
+		usuarioUpdate.setNombre(usuario.getNombre());
+		usuarioUpdate.setTipodocumento(usuario.getTipodocumento());
+		usuarioUpdate.setNumdocumento(usuario.getNumdocumento());
+		usuarioUpdate.setDireccion(usuario.getDireccion());
+		usuarioUpdate.setTelefono(usuario.getTelefono());
+		usuarioUpdate.setEmail(usuario.getEmail());
+		usuarioUpdate.setClave(usuario.getClave());
+		usuarioUpdate.setEstado(usuario.getEstado());
+		
+		return usuarioRepositorio.save(usuarioUpdate);
+	}
+
+	@Override
+	public Usuario deleteUsuario(Long id) {
+		Usuario usuarioDelete = getUsuario(id);
+		
+		if(usuarioDelete == null) {
+			return null;
+		}
+		
+		usuarioDelete.setNombre("ELIMINADO"); 
+		usuarioDelete.setTipodocumento("ELIMINADO");
+		usuarioDelete.setNumdocumento("ELIMINADO");
+		usuarioDelete.setDireccion("ELIMINADO");
+		usuarioDelete.setTelefono("ELIMINADO");
+		usuarioDelete.setEmail("ELIMINADO");
+		usuarioDelete.setClave("ELIMINADO");
+		usuarioDelete.setEstado(0);
+		
+		return usuarioRepositorio.save(usuarioDelete);
+	}
+
+	@Override
+	public List<Usuario> findByRoles(Roles rol) {
+		return usuarioRepositorio.findByRoles(rol);
+	}
+
+}
